b9089c0a727ccfebd14e28f0889e5a0c5a1fe5b4
PavelM2205 <pavelm2205@mail.ru>
2022-08-22 12:09:31 +0700

    Создан фильтр LoginFilter

---
+++ src/main/java/ru/job4j/filter/LoginFilter.java
@@ -0,0 +1,26 @@
+package ru.job4j.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class LoginFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        String uri = req.getRequestURI();
+        if (uri.endsWith("getTicket") && req.getSession().getAttribute("user_id") == null) {
+            req.getSession().setAttribute("mustLoginForTakeTicket", true);
+            res.sendRedirect(req.getContextPath() + "/loginPage");
+            return;
+        }
+        filterChain.doFilter(req, res);
+    }
+}
